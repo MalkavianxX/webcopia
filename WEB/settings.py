21795a12a1b08a3cdcb8f090687c5f9388dc0d9d
MalkavianxX <57018054+MalkavianxX@users.noreply.github.com>
2021-04-22 17:27:44 -0500

    Update settings.py

--- WEB/settings.py
+++ WEB/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+import sys
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
